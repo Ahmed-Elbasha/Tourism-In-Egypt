b7038531bba8d801ed3db67ddd1a49f42f6f9dc7
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-04-25 00:48:36 +0200

    Added Style to app

--- TourismInEgypt/app/src/main/java/com/example/ahmedelbasha/tourisminegypt/PlaceDetailsActivity.java
+++ TourismInEgypt/app/src/main/java/com/example/ahmedelbasha/tourisminegypt/PlaceDetailsActivity.java
@@ -17,6 +17,7 @@ import android.support.v4.view.WindowInsetsCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.Window;
@@ -24,6 +25,7 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 
 import com.jaeger.library.StatusBarUtil;
 
@@ -35,13 +37,11 @@ public class PlaceDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
         setContentView(R.layout.activity_place_details);
 
 //        ActionBar actionBar = getSupportActionBar();
 //        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00000000")));
 //        actionBar.setStackedBackgroundDrawable(new ColorDrawable(Color.parseColor("#00000000")));
-
 
         Intent receiveDataFromFragmentIntent = getIntent();
 
